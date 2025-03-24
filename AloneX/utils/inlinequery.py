0478a48061e7c163c9f57bfccff6637a1aa5840d
ramavtarchauhan420 <ramavtarchauhan420@gmail.com>
2025-03-24 20:43:17 +0530

    Update inlinequery.py

--- AloneX/utils/inlinequery.py
+++ AloneX/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
-            thumb_url="https://graph.org/file/ad99fd1fd7f3a99718080-be5bc325af6798b8ed.jpg",
+            thumb_url="https://graph.org/file/66b7bb1cf206376dd5ceb-03d047178a9c3418c9.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
